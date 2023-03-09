4afd4bc62324139597a47ad0e3af6fa1ec7f626d
soumesh2002 <soumesh6622@gmail.com>
2023-03-09 14:57:01 +0530

    added containers for notes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,35 @@
+import { useState } from "react";
+import { nanoid } from "nanoid";
 import NotesList from "./components/NotesList";
 
-const App = () => {
-  return <div className="container">
-    <NotesList />
-  </div>
-}
+const App = ({}) => {
+  const [notes, setNotes] = useState([
+    {
+      id: nanoid(),
+      text: "This is my first note",
+      date: "15/04/2021",
+    },
+    {
+      id: nanoid(),
+      text: "This is my second note",
+      date: "15/04/2021",
+    },
+    {
+      id: nanoid(),
+      text: "This is my third note",
+      date: "15/04/2021",
+    },
+  ]);
+
+  const addNote = (text) => {
+    console.log(text);
+  }
+
+  return (
+    <div className="container">
+      <NotesList notes={notes} handleAddNote={addNote} />
+    </div>
+  );
+};
 
 export default App;
