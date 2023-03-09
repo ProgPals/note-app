8a6440f5fa236504f8bb71d9e08c02f4ae933a18
soumesh2002 <soumesh6622@gmail.com>
2023-03-09 20:18:26 +0530

    final commit

--- src/components/AddNote.js
+++ src/components/AddNote.js
@@ -2,13 +2,20 @@ import { useState } from "react";
 
 const AddNote = ({ handleAddNote }) => {
   const [noteText, setNoteText] = useState("");
+  const characterLimit = 200;
 
   const handleChange = (e) => {
-    // console.log(e.target.value);
-    setNoteText(e.target.value);
+    if (characterLimit - e.target.value.length >= 0) {
+      setNoteText(e.target.value);
+    }
   };
 
-  const handleSaveClick = () => {};
+  const handleSaveClick = () => {
+    if (noteText.trim().length > 0) {
+      handleAddNote(noteText);
+      setNoteText("");
+    }
+  };
 
   return (
     <div className="note new">
@@ -20,9 +27,9 @@ const AddNote = ({ handleAddNote }) => {
         onChange={handleChange}
       ></textarea>
       <div className="note-footer">
-        <small>200 remanining</small>
+        <small>{characterLimit - noteText.length} Remanining</small>
         <button className="save" onClick={handleSaveClick}>
-          Save
+          Save ✅
         </button>
       </div>
     </div>
